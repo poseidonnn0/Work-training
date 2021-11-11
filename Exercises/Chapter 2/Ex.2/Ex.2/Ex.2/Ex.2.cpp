5e6de655a9b69724ea2e9028a24d402cc83e9322
Vitaliy <79005626141@mail.ru>
2021-11-11 20:53:06 +0200

    3 ex.

---
+++ Exercises/Chapter 2/Ex.2/Ex.2/Ex.2/Ex.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float Radius, area = 0, PI;
+    std::cout << "Введите радиус круга = ";
+    std::cin >> Radius;
+    PI = acos(-1); // Задаём число ПИ
+    area = Radius * Radius * PI;
+    std::cout << std::endl << "Площадь круга равна = " << area << std::endl;
+    return 0;
+}
+
---
+++ Exercises/Chapter 2/Ex.3/Ex.3/Ex.3/Ex.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::ifstream in("input.txt");
+	//std::ofstream out("output.txt");
+	setlocale(LC_ALL, "Rus");
+	std::string s;
+	int k = 0;
+	while (in >> s)
+	{
+		k++;
+	}
+	std::cout << "Количество слов в файле, разделённых пробелов = " << k << std::endl;
+	return 0;
+}
